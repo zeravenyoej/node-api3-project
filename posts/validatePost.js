b2f7159feed8dd6e0019e509ced7c91f47391c1a
Joey Nevarez <josephnevarez12@gmail.com>
2020-02-18 15:30:31 -0800

    wrote all validation files

--- posts/validatePost.js
+++ posts/validatePost.js
@@ -1,9 +1,16 @@
-module.exports = () => {
+const validatePost = () => {
     return (req, res, next) => {
-
+        if (!req.body) {
+            res.status(400).json({message: "missing post data"})
+        } else if (!req.body.text) {
+            res.status(400).json({message: "missing required text field"})
+        } else {
+            next()
+        }
     }
 }
 
+module.exports = validatePost
 
 /*  
 - `validatePost` validates the `body` on a request to create a new post
--- users/validateUser.js
+++ users/validateUser.js
@@ -1,9 +1,18 @@
-module.exports = () => {
+const db = require("./userDb")
+
+const validateUser = () => {
     return (req, res, next) => {
-        
+        if (!req.body) {
+            res.status(400).json({message: "missing user data"})
+        } else if (!req.body.name) {
+            res.status(400).json({message: "Missing required name field"})
+        } else {
+            next()
+        }
     }
 }
 
+module.exports = validateUser
 
 /*- `validateUser` validates the `body` on a request to create a new user
 - if the request `body` is missing, cancel the request and respond with status `400` 
